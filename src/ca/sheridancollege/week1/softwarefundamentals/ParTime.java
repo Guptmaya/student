255f59446a18269b833ecf29cf4d97b66d134872
guptmaya <guptmaya@sheridancollege.ca>
2019-05-23 13:00:05 -0400

    trying it out by myself for inclass

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/ParTime.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author hp
+ */
+public class ParTime {
+    
+        private int numClass;
+
+    /**
+     * Get the value of numClass
+     *
+     * @return the value of numClass
+     */
+    public int getNumClass() {
+        return numClass;
+    }
+
+    /**
+     * Set the value of numClass
+     *
+     * @param numClass new value of numClass
+     */
+    public void setNumClass(int numClass) {
+        this.numClass = numClass;
+    }
+
+    
+}
